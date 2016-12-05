9881bb9555ee84ca085558c44d3bb71f7e8d9e3b
Alex Lapinski <contact@alexlapinski.name>
2016-12-04 23:08:44 -0500

    Finished work with svdd

--- libsvm-scripts/water-treatment.sh
+++ libsvm-scripts/water-treatment.sh
@@ -11,7 +11,8 @@ SCALING_PARAMETERS="scale_parameters"
 SCALED_TRAINING_DATA="training_data.scale"
 SCALED_TEST_DATA="test_data.scale"
 
-MODEL_FILE=`pwd`/../models/water-treatment/libsvm/rbf.model
+RBF_MODEL_FILE=`pwd`/../models/water-treatment/libsvm/rbf.model
+HYPERPHERE_MODEL_FILE=`pwd`/../models/water-treatment/libsvm/hypersphere.model
 
 RBF_NU_VALUE=0.0625
 RBF_GAMMA_VALUE=0.00011094013184055919
@@ -37,11 +38,25 @@ echo ""
 
 echo "Training One-Class SVM"
 echo "Using RBF Kernel"
-./svm-train -s 2 -t 2 -g $RBF_GAMMA_VALUE -n $RBF_NU_VALUE -v 5 $SCALED_TRAINING_DATA $MODEL_FILE
+./svm-train -s 2 -t 2 -g $RBF_GAMMA_VALUE -n $RBF_NU_VALUE -v 5 $SCALED_TRAINING_DATA $RBF_MODEL_FILE
+echo ""
+
+echo "Training SVDD"
+./svm-train -s 5 -t 2 -g $RBF_GAMMA_VALUE -h $RBF_NU_VALUE -v 5 $SCALED_TRAINING_DATA $HYPERPHERE_MODEL_FILE
+echo ""
+
+echo "Testing One-Class SVM"
+./svm-predict $SCALED_TEST_DATA $RBF_MODEL_FILE $RBF_OUT_FILE out
+echo ""
+
+echo "Testing SVDD"
+./svm-predict $SCALED_TEST_DATA $HYPERPHERE_MODEL_FILE out
+echo ""
 
 # Remove Scaled Data
-#rm -f $SCALED_TRAINING_DATA
-#rm -r $SCALED_TEST_DATA
+rm -f $SCALED_TRAINING_DATA
+rm -r $SCALED_TEST_DATA
+rm -f out
 
 # Change directory back
 cd $START_PATH
